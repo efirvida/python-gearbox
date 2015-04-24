ce551197da5ed9e7ef3c75e2a044100ea4a489b7
Eduardo M. Fivida Donestevez <efirvida@gmail.com>
2015-04-24 16:38:01 -0700

    setup fixes

---
+++ README.md
@@ -0,0 +1,2 @@
+Python library for calculation of load capacities according to ISO and AGMA Standars
+See examples for usage
--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+version = '0.1.0'
 
 setup(
     name='python-gearbox',
-    version='0.01',
-    packages=['', 'gearbox'],
-    license='MIT',
-    author='efirvida',
+    version=version,
+    author='Eduardo M. Firvida Donestevez',
+    packages=find_packages(),
+    include_data_package=True,
     author_email='efirvida@gmail.com',
-    description='Python library for gear transmission design', requires=['numpy', 'scipy'],
+    description='Python library for gear transmission design',
+    requires=['numpy', 'scipy'],
     url='https://github.com/efirvida/python-gearbox',
     download_url='https://github.com/efirvida/python-gearbox/archive/master.zip',
     keywords=['gearbox', 'gear', 'agma', 'iso', 'gear transmission', 'engineering'],
+    platforms='any',
+    license='MIT',
+    zip_safe=False,
+    classifiers=['Intended Audience :: Developers',
+                 'Intended Audience :: Manufacturing',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.7',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+                 'Topic :: Software Development',
+                 'Topic :: Software Development :: Libraries',
+                 'Topic :: Software Development :: Libraries :: Application Frameworks'
+                 ]
 )
+
